2f5dc2d93a235326b5790a5929ca273f150e8ae6
julibglb <julia.bogolub@gmail.com>
2025-05-14 18:52:20 +0300

    Made select size pen

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 50;
 
+const button = document.querySelector('#button');
+const form = document.querySelector('#size');
+console.log(form.value)
+
+
 // creating new variable 
 let isDrawing = false;
 let lastX = 0;
@@ -33,17 +38,30 @@ function draw(e) {
         hue = 0;
     }
 
-    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-        direction = !direction;
-    }
+    // if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    //     direction = !direction;
+    // }
 
-    if (direction) {
-        ctx.lineWidth++;
-    } else {
-        ctx.lineWidth--;
-    }
+    // if (direction) {
+    //     ctx.lineWidth++;
+    // } else {
+    //     ctx.lineWidth--;
+    // }
+    
+
 }
 
+function changePen(size) {
+        console.log(size)
+        if (size === 'small') {
+            ctx.lineWidth = 15;
+        } else if (size === 'middle') {
+            ctx.lineWidth = 50;
+        } else if (size === 'large') {
+            ctx.lineWidth = 85;
+        } 
+
+    }
 
 // creating events listener
 canvas.addEventListener('mousedown', (e) => {
@@ -54,3 +72,9 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
+button.addEventListener('click', () => {
+     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+})
+
+form.addEventListener('change', () => changePen(form.value))
